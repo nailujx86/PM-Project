0c0dfe4b0ae63ec7b7ea744dd0a511b2cbc8dbb4
Julian B <julian.blazek@outlook.de>
2019-06-07 12:29:58 +0200

    Added User.java

---
+++ app/src/main/java/gr4/pm/dhbwverwaltung/objects/User.java
@@ -0,0 +1,92 @@
+package gr4.pm.dhbwverwaltung.objects;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+public class User {
+    private int id;
+    private String name;
+    private String email;
+    private String passwhash;
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPasswhash() {
+        return passwhash;
+    }
+
+    public void setPasswhash(String passwhash) {
+        this.passwhash = passwhash;
+    }
+
+    public User(int id, String name, String email, String passwhash) {
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.passwhash = passwhash;
+    }
+
+    public static ArrayList<User> parse(String json) {
+        ArrayList<User> users = new ArrayList<>();
+        JSONArray userArr = null;
+        try {
+            JSONObject obj = new JSONObject(json);
+            userArr = obj.getJSONArray("users");
+            for(int i = 0; i < userArr.length(); i++) {
+                JSONObject userDetail = userArr.getJSONObject(i);
+                String name = userDetail.getString("name");
+                String email = userDetail.getString("email");
+                int id = userDetail.getInt("id");
+                String passw = userDetail.getString("passwhash");
+                users.add(new User(id, name, email, passw));
+            }
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
+
+    public static String toJSON(ArrayList<User> users) {
+        JSONObject obj = new JSONObject();
+        JSONArray arr = new JSONArray();
+        try {
+            for(User user : users) {
+                JSONObject userobj = new JSONObject();
+                userobj.put("name", user.getName());
+                userobj.put("email", user.getEmail());
+                userobj.put("id", user.getId());
+                userobj.put("passwhash", user.getPasswhash());
+                arr.put(userobj);
+            }
+            obj.put("users", arr);
+        } catch (JSONException e) {
+            e.printStackTrace();
+        }
+        return obj.toString();
+    }
+}
