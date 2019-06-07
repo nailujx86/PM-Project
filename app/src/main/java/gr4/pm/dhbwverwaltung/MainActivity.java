f25930ec24bc95365ad83cecd8c1fa3ecf7ff3cd
Julian B <julian.blazek@outlook.de>
2019-06-07 12:25:53 +0200

    misc

--- app/src/main/java/gr4/pm/dhbwverwaltung/MainActivity.java
+++ app/src/main/java/gr4/pm/dhbwverwaltung/MainActivity.java
@@ -2,6 +2,7 @@ package gr4.pm.dhbwverwaltung;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import org.mindrot.jbcrypt.BCrypt;
 
 public class MainActivity extends AppCompatActivity {
 
