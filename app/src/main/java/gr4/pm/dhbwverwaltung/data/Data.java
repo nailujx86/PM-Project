f2059f33e273f0bc98d719542f43282a281d50d0
Julian <julian.blazek@outlook.de>
2019-06-12 12:23:36 +0200

    T2-4: Added data storage

---
+++ app/src/main/java/gr4/pm/dhbwverwaltung/data/Data.java
@@ -0,0 +1,25 @@
+package gr4.pm.dhbwverwaltung.data;
+
+import gr4.pm.dhbwverwaltung.objects.User;
+
+public class Data {
+    private static Data instance;
+    private Data(){};
+
+    private User curUser;
+
+    public static synchronized Data getInstance() {
+        if(Data.instance == null) {
+            Data.instance = new Data();
+        }
+        return Data.instance;
+    }
+
+    public User getUser() {
+        return curUser;
+    }
+
+    public void setUser(User user) {
+        curUser = user;
+    }
+}
