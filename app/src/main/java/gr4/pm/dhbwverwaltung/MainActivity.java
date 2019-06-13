120bb04e1be92b43d8a6fbb93ba3852f86adbe18
orifromtheblindforest <51700456+orifromtheblindforest@users.noreply.github.com>
2019-06-13 11:45:39 +0200

    Ansicht wecheln jetzt flüssig

--- app/src/main/java/gr4/pm/dhbwverwaltung/MainActivity.java
+++ app/src/main/java/gr4/pm/dhbwverwaltung/MainActivity.java
@@ -59,5 +59,6 @@ public class MainActivity extends AppCompatActivity {
         String password = editTextPassword.getText().toString();
         startActivity(intent); //wechselt Ansicht zu activity_menu
         finish();
+        overridePendingTransition(0,0);
     }
 }
--- app/src/main/java/gr4/pm/dhbwverwaltung/MenuActivity.java
+++ app/src/main/java/gr4/pm/dhbwverwaltung/MenuActivity.java
@@ -2,7 +2,11 @@ package gr4.pm.dhbwverwaltung;
 
         import android.content.Intent;
         import android.os.Bundle;
+        import android.view.View;
+        import android.widget.EditText;
         import android.widget.TextView;
+
+        import javax.security.auth.login.LoginException;
 
 public class MenuActivity extends MainActivity{
 
@@ -11,5 +15,10 @@ public class MenuActivity extends MainActivity{
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_menu);
     }
-
+    public void logoffUser(View view) {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent); //wechselt Ansicht zu activity_menu
+        finish();
+        overridePendingTransition(0,0);
+    }
 }
