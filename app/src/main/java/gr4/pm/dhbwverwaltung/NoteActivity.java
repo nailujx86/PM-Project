e1712eec23f12160c0ad00a8e2241d5505143263
Julian B <julian.blazek@outlook.de>
2019-06-18 23:34:41 +0200

    Added note functionality
    Not working yet

--- app/src/main/java/gr4/pm/dhbwverwaltung/NoteActivity.java
+++ app/src/main/java/gr4/pm/dhbwverwaltung/NoteActivity.java
@@ -1,11 +1,33 @@
 package gr4.pm.dhbwverwaltung;
 
 import android.os.Bundle;
+import android.support.v7.widget.RecyclerView;
 import android.view.Menu;
 
+import java.util.ArrayList;
+
+import gr4.pm.dhbwverwaltung.data.Data;
+import gr4.pm.dhbwverwaltung.io.FileIO;
+import gr4.pm.dhbwverwaltung.objects.Note;
+
 public class NoteActivity extends MainActivity {
+
+    ArrayList<Note> notes;
+
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_note);
+
+        if (FileIO.fileExists("/dhbwverwaltung/notes.json", false)) {
+            FileIO db = new FileIO("/dhbwverwaltung/notes.json", false);
+            String jsondb = db.readFromFile();
+            notes = Note.parse(jsondb);
+        } else {
+            notes = new ArrayList<>();
+            notes.add(new Note(1, Data.getInstance().getUser().getId(), "Keine Notizen gefunden"));
+        }
+
+        RecyclerView recyclerView = (RecyclerView) findViewById(R.id.recyclerView);
+
     }
 }
---
+++ app/src/main/java/gr4/pm/dhbwverwaltung/NoteAdapter.java
@@ -0,0 +1,63 @@
+package gr4.pm.dhbwverwaltung;
+
+import android.content.Context;
+import android.content.Intent;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import java.util.ArrayList;
+
+import gr4.pm.dhbwverwaltung.objects.Note;
+
+public class NoteAdapter extends RecyclerView.Adapter {
+
+    private Context context;
+    private ArrayList<Note> notes;
+
+    public NoteAdapter(Context context, ArrayList<Note> notes) {
+        this.context = context;
+        this.notes = notes;
+    }
+
+    @NonNull
+    @Override
+    public MyViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+        View v = LayoutInflater.from(viewGroup.getContext()).inflate(R.layout.row_layout_notes, viewGroup, false);
+        MyViewHolder vh = new MyViewHolder(v); // pass the view to View Holder
+        return vh;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull MyViewHolder viewHolder, int i) {
+        viewHolder.text.setText(notes.get(i).getContents());
+        viewHolder.itemView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                //Intent intent = new Intent(context, NoteDetail.class);
+                //intent.putExtra("noteid", notes.get(i).getId());
+                //context.startActivity(intent);
+                {}
+            }
+        });
+    }
+
+    @Override
+    public int getItemCount() {
+        return notes.size();
+    }
+
+    public class MyViewHolder extends ViewHolder {
+        TextView text;
+
+        public MyViewHolder(View itemView) {
+            super(itemView);
+            text = (TextView) itemView.findViewById(R.id.text);
+        }
+    }
+}
+}
