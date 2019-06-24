1d04abf5fac51ac39efa2c80944da2fc373156d1
orifromtheblindforest <51700456+orifromtheblindforest@users.noreply.github.com>
2019-06-24 20:56:37 +0200

    stay logged in (FUNKTIONIERT NOCH NICHT)

--- app/src/main/java/gr4/pm/dhbwverwaltung/MainActivity.java
+++ app/src/main/java/gr4/pm/dhbwverwaltung/MainActivity.java
@@ -1,11 +1,11 @@
 package gr4.pm.dhbwverwaltung;
 
 import android.content.Intent;
-import android.preference.EditTextPreference;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.CheckBox;
 import android.widget.EditText;
 
 import java.util.ArrayList;
@@ -24,6 +24,7 @@ public class MainActivity extends AppCompatActivity {
 
         setContentView(R.layout.activity_login);
 
+
         /* /// TESTS ///
         User user1 = new User(123, "terst", "test@test.de", Authentication.hashpw("abc"));
         User user2 = new User(124, "test2", "test2@test.de", Authentication.hashpw("abdef"));
@@ -40,21 +41,39 @@ public class MainActivity extends AppCompatActivity {
     private void initUserDatabase() {
         if (!FileIO.fileExists("/dhbwverwaltung/users.json", false)) {
             ArrayList<User> users = new ArrayList<>();
-            User user1 = new User(123, "test", "test@dhbw.de", Authentication.hashpw("test"));
-            User user2 = new User(124, "julian", "email@email.de", Authentication.hashpw("passwort"));
-            User user3 = new User(124, "moritz", "moritz@dhbw.de", Authentication.hashpw("pw"));
+            User user1 = new User(123, "test", "test@dhbw.de", Authentication.hashpw("test"),false);
+            User user2 = new User(124, "julian", "email@email.de", Authentication.hashpw("passwort"),false);
+            User user3 = new User(124, "moritz", "moritz@dhbw.de", Authentication.hashpw("pw"),false);
             users.add(user1);
             users.add(user2);
             users.add(user3);
             FileIO file = new FileIO("/dhbwverwaltung/users.json", false);
             file.writeToFile(User.toJSON(users));
             Log.i("dhbwverwaltung/userdb", "created");
-        } else {
+        } else {//check if there is a user with loggedIn Attribute
+            FileIO db = new FileIO("/dhbwverwaltung/users.json", false);
+            String jsondb = db.readFromFile();
+            ArrayList<User> users = User.parse(jsondb);
+            Data data = Data.getInstance();
+            for(User user : users) {
+                if(user.getStayLoggedIn().equals(true)) {
+                    Log.i("dhbwverwaltung/login", "user " + user.getEmail() + "is already logged in");
+                    data.setUser(user); //login User from database
+                    break;
+                }
+            }
+            Intent intent = new Intent(this, MenuActivity.class);
+            startActivity(intent); //wechselt Ansicht zu activity_menu
+            finish();
+            overridePendingTransition(0,0);
+
             Log.i("dhbwverwaltung/userdb", "exists");
         }
     }
 
     public void loginUser(View view){
+        CheckBox stayLoggedIn = findViewById(R.id.checkBox_stayLoggedIn);
+
         Intent intent = new Intent(this, MenuActivity.class);
         EditText editTextUserName = (EditText) findViewById(R.id.editText2);
         String username = editTextUserName.getText().toString(); // speichert username in
@@ -69,6 +88,9 @@ public class MainActivity extends AppCompatActivity {
             if(user.getEmail().equals(username)) {
                 Log.i("dhbwverwaltung/login", "found user " + user.getEmail());
                 Authentication.login(user, password);
+                if(stayLoggedIn.isChecked()){
+                    user.setStayLoggedIn(true);
+                }else user.setStayLoggedIn(false);
                 break;
             }
         }
